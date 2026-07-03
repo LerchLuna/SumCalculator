ff4699e826368a17df36cc4024b3a9bfac42c02e
LerchLuna <lunalerch0@gmail.com>
2026-07-03 13:00:47 +0800

    add 3rd number

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 function jsCalc(){
     const n1 =document.getElementById('num1').value;
     const n2 =document.getElementById('num2').value;
+    const n3 =document.getElementById('num3').value;
 
     const number1 =parseFloat(n1);
     const number2 =parseFloat(n2);
+    const number3 =parseFloat(n3);
 
     //get the sum
-    const sum = number1 + number2;
+    const sum = number1 + number2 + number3;
 
     document.getElementById('answer').textContent=sum;
 
@@ -15,6 +17,7 @@ function jsCalc(){
 function clearEntries(){
     document.getElementById('num1').value="";
     document.getElementById('num2').value="";
+    document.getElementById('num3').value="";
     document.getElementById('answer').value="";
 
 }
